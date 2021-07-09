12ae6ad5b22db877017bcea50e75124261ea896d
suman2023 <sumanmitra07@outlook.com>
2021-07-10 03:01:22 +0530

    initial build

---
+++ Hello_World/hello/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#There is a main urls.py for the whole project but we add for every app we create just for sake of simplicity and not cluttering the main urls.py
+urlpatterns = [
+    path("",views.index, name = "index"), #"" -> default route
+    path("name", views.name, name = "name") # this has url -> hello/name
+]
