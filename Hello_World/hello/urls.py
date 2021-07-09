1f3480811a9364329e9689340871e1b7c76e2214
suman2023 <sumanmitra07@outlook.com>
2021-07-10 03:26:19 +0530

    Created an App called Hello

--- Hello_World/hello/urls.py
+++ Hello_World/hello/urls.py
@@ -4,5 +4,7 @@ from . import views
 #There is a main urls.py for the whole project but we add for every app we create just for sake of simplicity and not cluttering the main urls.py
 urlpatterns = [
     path("",views.index, name = "index"), #"" -> default route
-    path("name", views.name, name = "name") # this has url -> hello/name
+    path("name", views.name, name = "name"), # this has url -> hello/name
+    path("<str:name>",views.greet, name = "greet"), # here we pass any string after hello/<here> we return with Hello <here>
+    
 ]
--- Hello_World/hello/views.py
+++ Hello_World/hello/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello!")
+def index(req): #req -> request
+    return render(req, "hello\index.html")
 
 def name(req):
     return HttpResponse("Hello, SUM4N!")
+
+def greet(req, name):
+    return render(req,"hello\greet.html",
+    {
+        "name": name.capitalize(),
+    }) # passing some parameters to the link as name in dict
