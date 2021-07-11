11dda668227af6a4abfaaefdfc657a6cbb9a42ea
suman2023 <sumanmitra07@outlook.com>
2021-07-12 00:34:42 +0530

    added a feature to add passenger who are not boarded to that particular flight but other

--- Hello_World/flights/urls.py
+++ Hello_World/flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "flights"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:flight_id>", views.flight, name="flight")
+    path("<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/book", views.bookFlight, name="bookFlight")
 ]
--- Hello_World/flights/views.py
+++ Hello_World/flights/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 # Create your views here.
 
@@ -9,7 +11,20 @@ def index(req):
 
 def flight(req, flight_id):
     flight = Flight.objects.get(id=flight_id)
-    return render(req, "flights/flight.html", {
-        "flight": flight,
-        "passengers": flight.passengers.all()
-    })
+    return render(
+        req, "flights/flight.html", {
+            "flight": flight,
+            "passengers": flight.passengers.all(),
+            "non_passengers": Passenger.objects.exclude(flights=flight).all()
+        })
+
+
+def bookFlight(req, flight_id):
+    if req.method == "POST":
+        flight = Flight.objects.get(id=flight_id)
+        passenger = Passenger.objects.get(
+            pk=int(req.POST["passenger"])
+        )  #we can use pk or id same thing as id is primary key but pk is more appropriate so from nxt pk
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(
+            reverse("flights:flight", args=(flight.id, )))
